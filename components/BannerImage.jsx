b53c690aaf809b769df748b6e03cd0eee0c6bdcd
Shrikrushna Patil <shri.medisage@gmail.com>
2024-01-08 18:03:23 +0530

    add changes

--- components/BannerImage.jsx
+++ components/BannerImage.jsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import React from "react";
-BannerImage
+import BannerImag from "../public/images/banner.png"
 function BannerImage() {
     return (
         <div className="relative pb-6 mx-4 mt-4">
             <Image
                 className="w-full h-64 sm:h-48 md:h-48 object-cover rounded-lg"
                 alt="Banner Image"
-                src="./images/banner.png"
+                src={BannerImag}
                 width={30}
                 height={30}
             />
