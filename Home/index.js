c631be996a9d23b62bc071e418e33dacb43b0db5
27n2003 <138979099+27n2003@users.noreply.github.com>
2024-01-08 18:53:49 +0530

    Add files via upload

---
+++ Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './Components/Home';
+import '@/styles/globals.css';
+
+const index = () => {
+  return (
+    <>
+        <Home/>
+    </>
+  )
+}
+
+export default index;
