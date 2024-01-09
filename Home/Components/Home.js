54cb4c2df13c6b92fec7679530261a03e924989c
27n2003 <138979099+27n2003@users.noreply.github.com>
2024-01-09 10:57:13 +0530

    Update Home.js

--- Home/Components/Home.js
+++ Home/Components/Home.js
@@ -5,7 +5,7 @@ import Footer from './Footer';
 import TopParent from './TopParent';
 import Author from './Author';
 import Category from './Category';
-// import ''
+import '@/styles/globals.css';
 
 const Home = () => {
   return (
