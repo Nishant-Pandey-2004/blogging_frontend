42f100705d10379a90b3fd5b17da0578484271f2
Karan Panda <108183567+karan-panda@users.noreply.github.com>
2024-01-13 13:40:15 +0530

    Update BlogForm.js

--- components/BlogForm.js
+++ components/BlogForm.js
@@ -1,6 +1,6 @@
 // components/BlogForm.js
 import React, { useState } from 'react';
-import { TagsInput } from "react-tag-input-component"; 
+import { TagsInput } from "react-tag-input-component";
 import dynamic from 'next/dynamic';
 
 const QuillEditor = dynamic(() => import('./QuillEditor'), { ssr: false });
@@ -27,7 +27,7 @@ const BlogForm = () => {
   const [metaDescription, setMetaDescription] = useState('');
   const [metaKeyword, setMetaKeyword] = useState('');
   const [image, setImage] = useState(null);
-  const [selected, setSelected] = useState([]); 
+  const [selected, setSelected] = useState([]);
 
   const handleImageChange = (selectedImage) => {
     setImage(selectedImage);
@@ -63,168 +63,207 @@ const BlogForm = () => {
     setImage(null);
   };
 
-  
-  
+
+
   const handleScrollToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
   return (
     <>
-    <h3 className="mx-20 mt-6">Editor  / </h3>
-        <hr/>
-    <div className={`p-4 ${styles.blogFormContainer}`}>
-      <div className='flex'>
-      <div className='bg-slate-200 rounded-xl p-5 w-1/2'>
+      <h3 className="mx-20 mt-6">Editor  / </h3>
+      <hr />
+      <div className={`p-4 ${styles.blogFormContainer}`}>
         <div className='flex'>
-        <label htmlFor="category" className={styles.requiredField}>
-          <strong>Category:</strong>
-        </label>&nbsp;&nbsp;&nbsp;&nbsp;
-        <select
-                id="category"
-                value={selectedCategory}
-                onChange={(e) => setSelectedCategory(e.target.value)}
-                className={styles.selectField}
-                required
-              >
-                <option value="" disabled>----------------------------SELECT--------------------------------</option>
-                {categories.map((cat) => (
-                  <optgroup label={cat.name} key={cat.name}>
-                    {cat.subcategories.map((subcat) => (
-                      <option key={subcat} value={`${cat.name} - ${subcat}`}>
-                        {subcat}
-                      </option>
+          <div className='bg-slate-200 rounded-xl p-5 w-full'>
+            <table className='w-full'>
+              {/* Category and Title */}
+              <tr>
+                <td className='p-4'>
+                  <label htmlFor="category" className={`${styles.requiredField} text-lg`}>
+                    <strong>Category:</strong>
+                  </label>
+                </td>
+                <td>
+                  <select
+                    id="category"
+                    value={selectedCategory}
+                    onChange={(e) => setSelectedCategory(e.target.value)}
+                    className={`${styles.selectField} text-lg`}
+                    required
+                  >
+                    <option value="" disabled>----------------------------SELECT--------------------------------</option>
+                    {categories.map((cat) => (
+                      <optgroup label={cat.name} key={cat.name}>
+                        {cat.subcategories.map((subcat) => (
+                          <option key={subcat} value={`${cat.name} - ${subcat}`}>
+                            {subcat}
+                          </option>
+                        ))}
+                      </optgroup>
                     ))}
-                  </optgroup>
-                ))}
-              </select>
-        </div>
-        <div className='flex'>
-        <label htmlFor="title" className={styles.requiredField}>
-          <strong>Title:</strong>
-        </label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <input
-          type="text"
-          id="title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          className={styles.inputField}
-          required
-          placeholder='Enter title here'
-        />
-        </div>
-        <div className='flex'>
-        <label htmlFor="description" className={styles.requiredField}>
-          <strong>Description:</strong>
-        </label>
-        <textarea
-          id="description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          className={styles.textareaField}
-          required
-          placeholder='Add a brief description....'
-        />
-        </div>       
-      </div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <div className='w-1/2'>
-        
-        <QuillEditor value={content} onChange={handleEditorChange} />
-      </div>
-      </div>  
-      <br></br>     
-      <hr className={`mb-4`} />
-      
-    
-      <div className='flex justify-center w-screen'>
-      <div className='w-1/2'><ImageUpload onImageChange={handleImageChange} /></div> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;    
-        <div className='w-1/2 space-y-28'>
-          <strong>Note:</strong>
-          <ol className={styles.greenList}>
-            <li>1. Thumbnail will be used for the front image of your blog.</li>
-            <li>2. Banner will be used as a cover page for your blog.</li>
-          </ol>
-        </div>
-      </div>
-      <br></br>
+                  </select>
+                </td>
 
-      <hr className={`mb-4`} />
+                <td>
+                  <label htmlFor="title" className={`${styles.requiredField} text-lg`}>
+                    <strong>Title:</strong>
+                  </label>
+                </td>
+                <td className='p-4'>
+                  <input
+                    type="text"
+                    id="title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    className={`${styles.inputField} text-lg`}
+                    required
+                    placeholder='Enter title here'
+                  />
+                </td>
+              </tr>
 
-      <div className='flex'>
-      <div className='bg-slate-200 text-black px-2 py-1 mt-4 rounded-xl w-1/2 shadow-md'> 
-  <h1><strong>Tags</strong></h1> <br/><br/>
-  <TagsInput 
-    value={selected} 
-    onChange={setSelected} 
-    name="tags"
-    placeHolder="Enter TAG..."
-  /> 
-  <div className="selected-tags">
-    {selected.map((tag, index) => (
-      <div key={index} className="tag bg-orange-400 text-yellow-300 px-2 py-1 rounded-md mr-2 mb-2 w-1/4">
-        {tag}
-      </div>
-    ))}
-  </div>
-</div>
+              {/* Description */}
+              <tr>
+                <td className='p-4'>
+                  <label htmlFor="description" className={`${styles.requiredField} text-lg`}>
+                    <strong>Description:</strong>
+                  </label>
+                </td>
 
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      
-        
-        <div className="bg-slate-200 text-black px-2 py-1 mt-4 rounded-xl w-1/2 shadow-md" >
-        <label className={styles.requiredField}>
-          <strong>SEO SETTINGS:</strong><br></br>
-        </label>
-          <label htmlFor="metaTitle" className={styles.requiredField}>
-            <h4>Meta Title:</h4>
-          </label>
-          <input
-            type="text"
-            id="metaTitle"
-            value={metaTitle}
-            onChange={(e) => setMetaTitle(e.target.value)}
-            className={styles.inputField}
-            required
-          />
-          <br />
-          <label htmlFor="metaDescription" className={styles.requiredField}>
-            <h4>Meta Description:</h4>
-          </label>
-          <textarea
-            id="metaDescription"
-            value={metaDescription}
-            onChange={(e) => setMetaDescription(e.target.value)}
-            className={styles.textareaField}
-            required
-          />
-          <br />
-          <label htmlFor="metaKeyword" className={styles.requiredField}>
-            <h4>Meta Keyword:</h4>
-          </label>
-          <input
-            type="text"
-            id="metaKeyword"
-            value={metaKeyword}
-            onChange={(e) => setMetaKeyword(e.target.value)}
-            className={styles.inputField}
-            required
-          />
+                <td colSpan={4}>
+                  <textarea
+                    id="description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    className={`${styles.textareaField} text-lg`}
+                    required
+                    placeholder='Add a brief description....'
+                  />
+                </td>
+              </tr>
+            </table>
+            <div className='flex'>
+              {/* Tags  */}
+              <div className='text-black p-4 mt-4 w-1/2'>
+                <h1 className='mb-2 text-xl'><strong>Tags</strong></h1>
+                <TagsInput
+                  value={selected}
+                  onChange={setSelected}
+                  name="tags"
+                  placeHolder="Enter TAG..."
+                />
+                <div className="selected-tags flex">
+                  {selected.map((tag, index) => (
+                    <div key={index} className="tag bg-orange-400 text-white text-lg p-0.5 pr-2 pl-2 m-2 mt-0 rounded-full mr-2 mb-2 w-fit">
+                      {tag}
+                    </div>
+                  ))}
+                </div>
+              </div>
+
+              <div className='w-1/2'>
+                <div className="mt-4 p-4">
+                  <label className={`${styles.requiredField} text-lg`}>
+                    <strong>SEO SETTINGS:</strong>
+                  </label>
+
+                  <table className="mt-2 w-full">
+                    <tbody>
+                      <tr>
+                        <td>
+                          <label htmlFor="metaTitle" className={`${styles.requiredField} mb-2`}>
+                            <h4>Meta Title:</h4>
+                          </label>
+                        </td>
+                        <td>
+                          <input
+                            type="text"
+                            id="metaTitle"
+                            value={metaTitle}
+                            onChange={(e) => setMetaTitle(e.target.value)}
+                            className={`w-full ${styles.inputField} text-lg`}
+                            required
+                          />
+                        </td>
+                      </tr>
+
+                      <tr>
+                        <td>
+                          <label htmlFor="metaDescription" className={`${styles.requiredField} mb-2`}>
+                            <h4>Meta Description:</h4>
+                          </label>
+                        </td>
+                        <td>
+                          <textarea
+                            id="metaDescription"
+                            value={metaDescription}
+                            onChange={(e) => setMetaDescription(e.target.value)}
+                            className={`w-full ${styles.textareaField} text-lg`}
+                            required
+                          />
+                        </td>
+                      </tr>
+
+                      <tr>
+                        <td>
+                          <label htmlFor="metaKeyword" className={`${styles.requiredField} mb-2`}>
+                            <h4>Meta Keyword:</h4>
+                          </label>
+                        </td>
+                        <td>
+                          <input
+                            type="text"
+                            id="metaKeyword"
+                            value={metaKeyword}
+                            onChange={(e) => setMetaKeyword(e.target.value)}
+                            className={`w-full ${styles.inputField} text-lg`}
+                            required
+                          />
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+
+            <h1 className='text-xl font-bold pl-4'>Upload Images:</h1>
+            <div className='flex p-4'>
+              <div className='flex items-center w-1/2 ml-12'>
+                <b><h4 className='mr-2 text-lg'>Thumbnail Image:</h4></b>
+                <div className='w-1/2 h-12 align-middle p-2 bg-white shadow'>
+                  <ImageUpload onImageChange={handleImageChange} />
+                </div>
+              </div>
+
+              <div className='flex items-center w-1/2 ml-12'>
+                <b><h4 className='mr-2 text-lg'>Banner Image:</h4></b>
+                <div className='w-1/2 h-12 align-middle p-2 bg-white shadow'>
+                  <ImageUpload onImageChange={handleImageChange} />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div className={`mb-4`}>
-        {/* Navigation Arrow to Scroll to Top */}
-        <div className={styles.scrollArrow} onClick={handleScrollToTop}>
-          &#x2191; {/* Unicode character for an upward arrow */}
+
+
+
+
+        <h2 className="text-2xl font-bold text-center p-8 pb-0">Write your Blogs here</h2>
+        <div className="ml-40 mr-40 mt-10">
+          <QuillEditor value={content} onChange={handleEditorChange} />
         </div>
-      </div>
-      <br/><br/>
-      {/* Submit Button */}
-      <button className={`bg-green-500 font-bold text-white px-4 py-2 mt-4 rounded-lg flex items-center mx-auto`} onClick={handleSubmit}>
-        CREATE
-      </button>
 
-    </div>
+
+        <br /><br />
+        {/* Submit Button */}
+        <button className={`bg-green-500 font-bold text-white px-4 py-2 mt-4 rounded-lg flex items-center mx-auto`} onClick={handleSubmit}>
+          CREATE
+        </button>
+
+      </div>
     </>
   );
 };
