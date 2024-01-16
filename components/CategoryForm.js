bfe08a2753ec234ab1f2fa27864b2aa26dedef8a
Nishant Pandey <113779980+Nishant-Pandey-2004@users.noreply.github.com>
2024-01-16 15:47:43 +0530

    Update CategoryForm.js

--- components/CategoryForm.js
+++ components/CategoryForm.js
@@ -135,56 +135,59 @@ const CatForm = () => {
                 </td>
               </tr>
             </table>
-            <div className={`p-4`}>
-              <h1 className='text-xl font-bold pl-4'>Upload Images:</h1>
-              <div className='p-4'>
-                <div className='items-center ml-12  mr-40'>
-                  <b><h4 className='mr-2 text-lg'>Thumbnail Image:</h4></b>
-                  <div className='w-1/2 h-12 align-middle p-2 bg-white shadow'>
-                    <ImageUpload onImageChange={handleImageChange} />
-                  </div>
+            <div className='flex'>
+              <div className='text-black p-4 mt-4 w-1/2'>
+                <h1 className='mb-2 text-xl'><strong>Tags</strong></h1>
+                <TagsInput
+                  value={selected}
+                  onChange={setSelected}
+                  name="tags"
+                  placeHolder="Enter TAG..."
+                />
+                <div className="selected-tags flex">
+                  {selected.map((tag, index) => (
+                    <div key={index} className="tag bg-orange-400 text-white text-lg p-0.5 pr-2 pl-2 m-2 mt-0 rounded-full mr-2 mb-2 w-fit">
+                      {tag}
+                    </div>
+                  ))}
                 </div>
+              </div>
 
-                <div className='items-center ml-12 mr-40'>
-                  <b><h4 className='mr-2 text-lg'>Banner Image:</h4></b>
-                  <div className='w-1/2 h-12 align-middle p-2 bg-white shadow'>
-                    <ImageUpload onImageChange={handleImageChange} />
+              <div className={`p-4`}>
+                <h1 className='text-xl font-bold pl-4'>Upload Images:</h1>
+                <div className='p-4'>
+                  <div className='items-center ml-12  mr-40'>
+                    <b><h4 className='mr-2 text-lg'>Front Image:</h4></b>
+                    <div className=' h-12 align-middle p-2 bg-white shadow'>
+                      <ImageUpload onImageChange={handleImageChange} />
+                    </div>
+                  </div>
+
+                  <div className='items-center ml-12 mr-40'>
+                    <b><h4 className='mr-2 text-lg'>Cover Image:</h4></b>
+                    <div className=' h-12 align-middle p-2 bg-white shadow'>
+                      <ImageUpload onImageChange={handleImageChange} />
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
 
-            <div className='text-black p-4 mt-4 w-1/2'>
-              <h1 className='mb-2 text-xl'><strong>Tags</strong></h1>
-              <TagsInput
-                value={selected}
-                onChange={setSelected}
-                name="tags"
-                placeHolder="Enter TAG..."
-              />
-              <div className="selected-tags flex">
-                {selected.map((tag, index) => (
-                  <div key={index} className="tag bg-orange-400 text-white text-lg p-0.5 pr-2 pl-2 m-2 mt-0 rounded-full mr-2 mb-2 w-fit">
-                    {tag}
-                  </div>
-                ))}
-              </div>
-            </div>
 
 
 
-            </div></div>
+          </div></div>
 
 
-            {/* Submit Button */}
-            <button className={`bg-green-500 font-bold text-white px-4 py-2 mt-4 rounded-lg flex items-center m-12`} onClick={handleSubmit}>
-              CREATE CATEGORY
-            </button>
+        {/* Submit Button */}
+        <button className={`bg-green-500 font-bold text-white px-4 py-2 mt-4 rounded-lg flex items-center m-12`} onClick={handleSubmit}>
+          CREATE CATEGORY
+        </button>
 
-          </div>
-          
-        </>
-        );
+      </div>
+
+    </>
+  );
 };
 
-        export default CatForm;
+export default CatForm;
