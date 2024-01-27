30ec36f330a6ee8a3fcf2a4262d006863fa2b551
Omkar Kotekar <139045510+OmkarKotekar@users.noreply.github.com>
2024-01-27 13:20:17 +0530

    registration form page

---
+++ pages/register/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+
+import Register from '../../components/Register'
+
+const register = () => {
+    return <>
+    <div className='overflow-hidden'>
+        <div className='flex justify-center'>
+            <Image src={'/logo.png'} width={500} height={500}/>
+        </div>
+        <div className=''><Register/></div>
+        </div>
+    </>
+}
+
+export default register
